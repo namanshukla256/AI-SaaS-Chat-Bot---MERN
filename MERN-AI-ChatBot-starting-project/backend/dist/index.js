b6aeedb1770305c51474efcdea5e29d7f6d57ece
Naman Shukla <namanshukla256@gmail.com>
2025-01-04 23:53:33 +0530

    4/1

---
+++ MERN-AI-ChatBot-starting-project/backend/dist/index.js
@@ -0,0 +1,3 @@
+import express from 'express'; // import express
+var app = express(); // create express app
+app.listen(5001, function () { return console.log('server started'); });
---
+++ MERN-AI-ChatBot-starting-project/backend/src/index.ts
@@ -0,0 +1,5 @@
+import express from 'express'; // import express
+
+const app = express();  // create express app
+
+app.listen(5001, () => console.log('server started'));
